d5a48cb2bf3862d29f0a208986212332a1ac79ee
安超 <240853850@qq.com>
2020-04-17 15:54:21 +0800

    [excel] 公式处理

--- src/test/java/com/ac/excel/FormulaTest.java
+++ src/test/java/com/ac/excel/FormulaTest.java
@@ -1,9 +1,15 @@
 package com.ac.excel;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.util.CellReference;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author anchao
@@ -16,24 +22,46 @@ public class FormulaTest {
      */
     @Test
     public void test() throws IOException {
+
+        MeData meData1 = new MeData(0, "A", "1");
+        MeData meData2 = new MeData(1, "A", "2");
+        List<MeData> meDataList = Arrays.asList(meData1,meData2);
+        meDataList= new ArrayList<>(meDataList);
+
+
         Workbook workbook = WorkbookFactory.create(true);
-        Sheet sheet = workbook.createSheet("test");
-        Row row = sheet.createRow(0);
-        row.createCell(0).setCellValue(1);
-        row.createCell(1).setCellValue(2);
-        row.createCell(2).setCellValue(3);
-        row.createCell(3).setCellValue(4);
-        row.createCell(4).setCellValue(5);
-        Cell cell = sheet.getRow(0).createCell(5);
-       // String colString = CellReference.convertNumToColString(0);  //长度转成ABC列
-        cell.setCellFormula("=sum(a1:e1)".replaceAll("=",""));
-        //sheet.setForceFormulaRecalculation(true);//强制执行新设置的公式
-        //按公式计算
-        FormulaEvaluator evaluator = cell.getSheet().getWorkbook().getCreationHelper().createFormulaEvaluator();
+        Sheet sheet = workbook.createSheet();
+        for (MeData meData : meDataList) {
+            int row_index = meData.getRow();
+            String col = meData.getCol();
+            String value = meData.getValue();
+            if (sheet.getRow(row_index)!=null) {
+                Row row = sheet.getRow(row_index);
+                int col_index = CellReference.convertColStringToIndex(col);
+                row.createCell(col_index).setCellValue(Double.valueOf(value));
+            }
+            else{
+                Row row = sheet.createRow(row_index);
+                int col_index = CellReference.convertColStringToIndex(col);
+                row.createCell(col_index).setCellValue(Double.valueOf(value));
+            }
+        }
+        int rowNumNew = sheet.getLastRowNum()+1;
+        Row row = sheet.createRow(rowNumNew);
+        Cell cell = row.createCell(0);
+        FormulaEvaluator evaluator = sheet.getWorkbook().getCreationHelper().createFormulaEvaluator();
+        cell.setCellFormula("IF(A1<A2,10,20)");
         evaluator.evaluateFormulaCell(cell);
         CellValue cellValue = evaluator.evaluate(cell);
-        double celldata = cellValue.getNumberValue();
-        System.out.println("--> value: " + celldata);
-        //workbook.write(new FileOutputStream("C:\\Users\\anchao\\Desktop\\formula.xlsx"));
+        double data = cellValue.getNumberValue();
+        System.out.println("--> data: " + data);
+    }
+
+    @Data
+    @AllArgsConstructor
+    public class MeData{
+        private int row;
+        private String col;
+        private String value;
     }
 }
