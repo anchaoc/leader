df83b75b23a87b9fe623c179ae152417c0616448
安超 <240853850@qq.com>
2020-04-17 16:36:47 +0800

    [excel] 公式处理

--- src/test/java/com/ac/excel/FormulaTest.java
+++ src/test/java/com/ac/excel/FormulaTest.java
@@ -2,6 +2,7 @@ package com.ac.excel;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.apache.commons.lang3.ObjectUtils;
 import org.apache.poi.ss.formula.FormulaParseException;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.ss.util.CellReference;
@@ -26,8 +27,8 @@ public class FormulaTest {
 
         MeData meData1 = new MeData(0, "A", "1");
         MeData meData2 = new MeData(1, "A", "2");
-        List<MeData> meDataList = Arrays.asList(meData1,meData2);
-        meDataList= new ArrayList<>(meDataList);
+        List<MeData> meDataList = Arrays.asList(meData1, meData2);
+        meDataList = new ArrayList<>(meDataList);
 
 
         Workbook workbook = null;
@@ -38,18 +39,17 @@ public class FormulaTest {
                 int row_index = meData.getRow();
                 String col = meData.getCol();
                 String value = meData.getValue();
-                if (sheet.getRow(row_index)!=null) {
+                if (sheet.getRow(row_index) != null) {
                     Row row = sheet.getRow(row_index);
                     int col_index = CellReference.convertColStringToIndex(col);
                     row.createCell(col_index).setCellValue(Double.valueOf(value));
-                }
-                else{
+                } else {
                     Row row = sheet.createRow(row_index);
                     int col_index = CellReference.convertColStringToIndex(col);
                     row.createCell(col_index).setCellValue(Double.valueOf(value));
                 }
             }
-            int rowNumNew = sheet.getLastRowNum()+1;
+            int rowNumNew = sheet.getLastRowNum() + 1;
             Row row = sheet.createRow(rowNumNew);
             Cell cell = row.createCell(0);
             FormulaEvaluator evaluator = sheet.getWorkbook().getCreationHelper().createFormulaEvaluator();
@@ -61,15 +61,16 @@ public class FormulaTest {
         } catch (IOException | NumberFormatException | FormulaParseException e) {
             e.printStackTrace();
         } finally {
-            assert workbook != null;
-            workbook.close();
+            if (ObjectUtils.isNotEmpty(workbook)) {
+                workbook.close();
+            }
         }
 
     }
 
     @Data
     @AllArgsConstructor
-    public class MeData{
+    public class MeData {
         private int row;
         private String col;
         private String value;
