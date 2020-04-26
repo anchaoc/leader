db90ae4a3f1abfa46fb6d8c046d9b33519d88ff6
安超 <240853850@qq.com>
2020-04-26 19:14:02 +0800

    [easy excel] 写

--- src/test/java/com/ac/easyexcel/EasyExcelTest.java
+++ src/test/java/com/ac/easyexcel/EasyExcelTest.java
@@ -2,15 +2,19 @@ package com.ac.easyexcel;
 
 import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.ExcelReader;
+import com.alibaba.excel.annotation.ExcelProperty;
 import com.alibaba.excel.context.AnalysisContext;
 import com.alibaba.excel.event.AnalysisEventListener;
 import com.alibaba.excel.read.metadata.holder.ReadRowHolder;
 import com.alibaba.excel.read.metadata.holder.ReadSheetHolder;
 import com.alibaba.excel.write.metadata.style.WriteCellStyle;
+import com.alibaba.excel.write.metadata.style.WriteFont;
 import com.alibaba.excel.write.style.HorizontalCellStyleStrategy;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.VerticalAlignment;
 import org.apache.poi.ss.util.CellReference;
 import org.junit.Test;
 
@@ -31,17 +35,45 @@ public class EasyExcelTest {
      * 测试写入
      */
     @Test
-    public void excelTest3(){
-        List<WriteData> writeDataList = Collections.singletonList(new WriteData("a", 16));
-        WriteCellStyle writeCellStyle = new WriteCellStyle();
-        writeCellStyle.setBottomBorderColor(IndexedColors.GOLD.index);
-        HorizontalCellStyleStrategy horizontalCellStyleStrategy = new HorizontalCellStyleStrategy(writeCellStyle,writeCellStyle);
-        EasyExcel
-                .write("C:\\Users\\anchao\\Desktop\\"+System.currentTimeMillis()+".xlsx", WriteData.class)
-                .useDefaultStyle(false)
+    public void importExcelTest(){
+        File file = new File("C:\\Users\\anchao\\Desktop\\test.xlsx");
+        ImportTest importTest = new ImportTest();
+        importTest.setName("测试");
+        List<ImportTest> importTestList = Collections.singletonList(importTest);
+        // 头的策略
+        WriteCellStyle headWriteCellStyle = new WriteCellStyle();
+        // 背景色
+        headWriteCellStyle.setFillForegroundColor(IndexedColors.PALE_BLUE.getIndex());
+        WriteFont headWriteFont = new WriteFont();
+        headWriteFont.setFontHeightInPoints((short) 12);
+        headWriteCellStyle.setWriteFont(headWriteFont);
+        // 内容的策略
+        WriteCellStyle contentWriteCellStyle = new WriteCellStyle();
+        // 这里需要指定 FillPatternType 为FillPatternType.SOLID_FOREGROUND 不然无法显示背景颜色.头默认了 FillPatternType所以可以不指定
+        // contentWriteCellStyle.setFillPatternType(FillPatternType.SOLID_FOREGROUND);
+        // 背景绿色
+        //contentWriteCellStyle.setFillForegroundColor(IndexedColors.GREEN.getIndex());
+        //边框
+        //contentWriteCellStyle.setBorderBottom(BorderStyle.THIN);
+        //contentWriteCellStyle.setBorderRight(BorderStyle.THIN);
+        // 字体策略
+        WriteFont contentWriteFont = new WriteFont();
+        // 字体大小
+        contentWriteFont.setFontHeightInPoints(Short.valueOf("12"));
+        contentWriteCellStyle.setWriteFont(contentWriteFont);
+        //设置 自动换行
+        contentWriteCellStyle.setWrapped(true);
+        //设置 垂直居中
+        contentWriteCellStyle.setVerticalAlignment(VerticalAlignment.CENTER);
+        HorizontalCellStyleStrategy horizontalCellStyleStrategy = new HorizontalCellStyleStrategy(headWriteCellStyle, contentWriteCellStyle);
+        //写
+        EasyExcel.write(file)
+                .sheet("审计分录")
+                .head(ImportTest.class)
                 .registerWriteHandler(horizontalCellStyleStrategy)
-                .sheet("sheet1")
-                .doWrite(writeDataList);
+                .table(1)
+                .doWrite(importTestList);
+
     }
 
     /**
@@ -167,4 +199,11 @@ public class EasyExcelTest {
             }
         }
     }
+}
+
+
+@Data
+class ImportTest {
+    @ExcelProperty(value = "名称",index = 0)
+    private String name;
 }
