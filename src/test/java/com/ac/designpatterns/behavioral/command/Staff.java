9418a99515e41535db3330882f55fcb0bd74dd8e
安超 <240853850@qq.com>
2020-09-07 17:57:49 +0800

    [设计模式]add

--- src/test/java/com/ac/designpatterns/behavioral/command/Staff.java
+++ src/test/java/com/ac/designpatterns/behavioral/command/Staff.java
@@ -1,14 +1,14 @@
 package com.ac.designpatterns.behavioral.command;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * @author anchao
  * @date 2020/9/7 17:49
  **/
 public class Staff {
-    private List<Command> commandList = new ArrayList<Command>();
+    private List<Command> commandList = new CopyOnWriteArrayList<Command>();
 
     public void addCommand(Command command){
         commandList.add(command);
@@ -17,5 +17,6 @@ public class Staff {
     public void executeCommands(){
         //遍历执行命令
         commandList.forEach(Command::execute);
+        commandList.clear();
     }
 }
