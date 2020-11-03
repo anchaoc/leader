0daac61636f4851bb11811425cdaa242edf9923b
安超 <240853850@qq.com>
2020-11-03 10:19:25 +0800

    jdk juc cyclicbarrier ok

--- src/test/java/com/ac/jdk/juc/flowcontrol/cyclicbarrier/CyclicBarrierDemo.java
+++ src/test/java/com/ac/jdk/juc/flowcontrol/cyclicbarrier/CyclicBarrierDemo.java
@@ -8,12 +8,7 @@ import java.util.concurrent.CyclicBarrier;
  */
 public class CyclicBarrierDemo {
     public static void main(String[] args) {
-        CyclicBarrier cyclicBarrier = new CyclicBarrier(5, new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("所有人都到场了， 大家统一出发！");
-            }
-        });
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(5, () -> System.out.println("所有人都到场了， 大家统一出发！"));
         for (int i = 0; i < 10; i++) {
             new Thread(new Task(i, cyclicBarrier)).start();
         }
